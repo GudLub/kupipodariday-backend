00765c7eb9f98429e0c0870feff548cc868eeac1
GudLub <gudilinalu@yandex.ru>
2024-05-13 13:53:42 +0300

    first review

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,37 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { WishlistModule } from './wishlist/wishlist.module';
+import { WishModule } from './wish/wish.module';
+import { UserModule } from './user/user.module';
+import { OffersModule } from './offers/offers.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    AuthModule,
+    WishlistModule,
+    WishModule,
+    UserModule,
+    OffersModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        synchronize: false,
+        entities: [__dirname + '/**/*.entity{.js, .ts}'],
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+
+  controllers: [],
   providers: [],
 })
 export class AppModule {}
